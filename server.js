fb4df42b6aee9fa7da4a16ecb086eda0569dc52b
ravindersharma83 <ravinder200198@gmail.com>
2025-01-30 18:56:27 +0530

    static middleware routing

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'public') + '/index.html');
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname,'public') + '/index.html');
+// })
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'public') + '/about.html');
-})
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.resolve(__dirname,'public') + '/about.html');
+// })
+
+// Static routing using static express middleware
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`listening on PORT ${PORT}`);
