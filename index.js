60438cc384cdf44b0bf485f40d7970bbeeccc4d0
ravindersharma83 <ravinder200198@gmail.com>
2025-01-30 18:01:50 +0530

    Basic routing using HTTP Module

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Welcome to Node");
+// HTTP Module
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+// create a server
+const app = http.createServer((req, res) => {
+    console.log(req.url);
+    
+    // res.end("Welcome to Node");
+    if(req.url === '/') {
+        fs.readFile(path.join(__dirname, "public", "index.html"), (err, content) => {
+            if(err) {
+                throw err;
+            }
+            res.end(content)
+        })
+    } else if(req.url === '/about') {
+        fs.readFile(path.join(__dirname, "public", "about.html"), (err, content) => {
+            if(err) {
+                throw err;
+            }
+            res.end(content)
+        })
+    }
+})
+
+
+// starting a server on a particular port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`);
+    
+})
+
